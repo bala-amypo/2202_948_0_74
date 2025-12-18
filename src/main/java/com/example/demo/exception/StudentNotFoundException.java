505c7249182f26e47a68049593195f67a16396a4
bala-amypo <bala@amypo.in>
2025-12-18 08:32:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/StudentNotFoundException.java
+++ src/main/java/com/example/demo/exception/StudentNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.demo.exception;
  public class StudentNotFoundException extends RuntimeException{
-    
+    public StudentNotFoundException(String errMsg){
+        super(errMsg);
+        
+    }
  }
